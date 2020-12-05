721f9a39173c9ea3e7f307f6e423d01379cddd91
Kaww <rmluux@gmail.com>
2020-12-05 18:32:39 +0100

    Initial Commit

---
+++ SpaceXplorer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpaceXplorer
+//
+//  Created by KAWRANTIN LE GOFF on 05/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpaceXplorer/SpaceXplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXplorerApp.swift
+//  SpaceXplorer
+//
+//  Created by KAWRANTIN LE GOFF on 05/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
